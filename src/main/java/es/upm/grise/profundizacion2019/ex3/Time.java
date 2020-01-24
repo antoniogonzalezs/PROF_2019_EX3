a670a3897c25dcf2a3c77088340c1cee73df5c76
Antonio Gonzalez Sanz <kcalorace87@hotmail.com>
2020-01-24 16:37:52 +0100

    Finalizada la prueba con Mockito

--- src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/Time.java
@@ -5,13 +5,13 @@ import java.time.format.DateTimeFormatter;
 
 public class Time {
 	
-	LocalDateTime startDate;
+	static LocalDateTime startDate;
 	
 	public Time(LocalDateTime startDate) {
 		this.startDate = startDate;
 	}
 
-	public String getFutureTime(long seconds) {
+	public static String getFutureTime(long seconds) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		LocalDateTime time2 = startDate.plusSeconds(seconds);
 		return formatter.format(time2);	
--- src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
@@ -1,6 +1,7 @@
 package es.upm.grise.profundizacion2019.ex3;
 
 import org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,6 +14,7 @@ import org.junit.Test;
 public class smokeTest {
 	MyClass my;
 	Time time;
+	Time mockitoTime;
 	static final String EXPECTED_RESULT = "2020/01/01 00:02:00";
 	static final LocalDateTime START_DATE = LocalDateTime.of(2020, 1, 1, 0, 0, 0, 0);
 	
@@ -20,6 +22,7 @@ public class smokeTest {
 	public void setUp() {
 		my = new MyClass(START_DATE);
 		time = new Time(START_DATE);
+		mockitoTime = mock(Time.class);
 	}
 
 	@Test
@@ -35,5 +38,13 @@ public class smokeTest {
 		long SECONDS = 120;
 		String RETURNED = time.getFutureTime(SECONDS);
 		assertEquals(EXPECTED_RESULT, RETURNED);
+	}
+	
+	/* TEST MOCKITO */
+	@Test
+	public void mockitoTest() {
+		long SECONDS = 120;
+		my.nextTime(SECONDS);
+		verify(mockitoTime, times(1)).getFutureTime(SECONDS);
 	}
 }
